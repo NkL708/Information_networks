4cb0b7df0ed58584300e54028541b1e6fef04f0d
NkL <nklnsk708@gmail.com>
2022-02-24 23:37:21 +0700

    Lab1 begin

--- Information_networks/Form1.Designer.cs
+++ Information_networks/Form1.Designer.cs
@@ -28,13 +28,141 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.initButton = new System.Windows.Forms.Button();
+            this.openSessionButton = new System.Windows.Forms.Button();
+            this.closeSessionButton = new System.Windows.Forms.Button();
+            this.executeButton = new System.Windows.Forms.Button();
+            this.connectionLabel = new System.Windows.Forms.Label();
+            this.passwordTextBox = new CueTextBox();
+            this.serverPortTextBox = new CueTextBox();
+            this.serverAdressTextBox = new CueTextBox();
+            this.loginTextBox = new CueTextBox();
+            this.SuspendLayout();
+            // 
+            // initButton
+            // 
+            this.initButton.Location = new System.Drawing.Point(54, 60);
+            this.initButton.Name = "initButton";
+            this.initButton.Size = new System.Drawing.Size(130, 25);
+            this.initButton.TabIndex = 0;
+            this.initButton.Text = "Инициализация";
+            this.initButton.UseVisualStyleBackColor = true;
+            this.initButton.Click += new System.EventHandler(this.initButtonClick);
+            // 
+            // openSessionButton
+            // 
+            this.openSessionButton.Location = new System.Drawing.Point(54, 106);
+            this.openSessionButton.Name = "openSessionButton";
+            this.openSessionButton.Size = new System.Drawing.Size(130, 25);
+            this.openSessionButton.TabIndex = 1;
+            this.openSessionButton.Text = "Открытие сеанса";
+            this.openSessionButton.UseVisualStyleBackColor = true;
+            this.openSessionButton.Click += new System.EventHandler(this.openSessionButtonClick);
+            // 
+            // closeSessionButton
+            // 
+            this.closeSessionButton.Location = new System.Drawing.Point(54, 156);
+            this.closeSessionButton.Name = "closeSessionButton";
+            this.closeSessionButton.Size = new System.Drawing.Size(130, 25);
+            this.closeSessionButton.TabIndex = 2;
+            this.closeSessionButton.Text = "Закрытие сеанса";
+            this.closeSessionButton.UseVisualStyleBackColor = true;
+            this.closeSessionButton.Click += new System.EventHandler(this.closeSessionButtonClick);
+            // 
+            // executeButton
+            // 
+            this.executeButton.Location = new System.Drawing.Point(54, 207);
+            this.executeButton.Name = "executeButton";
+            this.executeButton.Size = new System.Drawing.Size(130, 25);
+            this.executeButton.TabIndex = 3;
+            this.executeButton.Text = "Выполнение команды";
+            this.executeButton.UseVisualStyleBackColor = true;
+            this.executeButton.Visible = false;
+            this.executeButton.Click += new System.EventHandler(this.executeButtonClick);
+            // 
+            // connectionLabel
+            // 
+            this.connectionLabel.AutoSize = true;
+            this.connectionLabel.ForeColor = System.Drawing.Color.Red;
+            this.connectionLabel.Location = new System.Drawing.Point(374, 136);
+            this.connectionLabel.Name = "connectionLabel";
+            this.connectionLabel.Size = new System.Drawing.Size(85, 13);
+            this.connectionLabel.TabIndex = 12;
+            this.connectionLabel.Text = "Не подключено";
+            // 
+            // passwordTextBox
+            // 
+            this.passwordTextBox.Cue = "Пароль";
+            this.passwordTextBox.Location = new System.Drawing.Point(211, 109);
+            this.passwordTextBox.Name = "passwordTextBox";
+            this.passwordTextBox.PasswordChar = '*';
+            this.passwordTextBox.Size = new System.Drawing.Size(130, 20);
+            this.passwordTextBox.TabIndex = 5;
+            this.passwordTextBox.Visible = false;
+            // 
+            // serverPortTextBox
+            // 
+            this.serverPortTextBox.Cue = "Порт";
+            this.serverPortTextBox.Location = new System.Drawing.Point(211, 210);
+            this.serverPortTextBox.Name = "serverPortTextBox";
+            this.serverPortTextBox.Size = new System.Drawing.Size(130, 20);
+            this.serverPortTextBox.TabIndex = 7;
+            this.serverPortTextBox.Text = "5432";
+            this.serverPortTextBox.Visible = false;
+            // 
+            // serverAdressTextBox
+            // 
+            this.serverAdressTextBox.Cue = "Адрес сервера";
+            this.serverAdressTextBox.Location = new System.Drawing.Point(211, 159);
+            this.serverAdressTextBox.Name = "serverAdressTextBox";
+            this.serverAdressTextBox.Size = new System.Drawing.Size(130, 20);
+            this.serverAdressTextBox.TabIndex = 6;
+            this.serverAdressTextBox.Text = "localhost";
+            this.serverAdressTextBox.Visible = false;
+            // 
+            // loginTextBox
+            // 
+            this.loginTextBox.Cue = "Логин";
+            this.loginTextBox.Location = new System.Drawing.Point(211, 63);
+            this.loginTextBox.Name = "loginTextBox";
+            this.loginTextBox.Size = new System.Drawing.Size(130, 20);
+            this.loginTextBox.TabIndex = 4;
+            this.loginTextBox.Visible = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.BackColor = System.Drawing.Color.DimGray;
+            this.ClientSize = new System.Drawing.Size(484, 286);
+            this.Controls.Add(this.passwordTextBox);
+            this.Controls.Add(this.connectionLabel);
+            this.Controls.Add(this.serverPortTextBox);
+            this.Controls.Add(this.serverAdressTextBox);
+            this.Controls.Add(this.loginTextBox);
+            this.Controls.Add(this.executeButton);
+            this.Controls.Add(this.closeSessionButton);
+            this.Controls.Add(this.openSessionButton);
+            this.Controls.Add(this.initButton);
+            this.KeyPreview = true;
+            this.Name = "Form1";
+            this.Text = "Джавы-джуны";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button initButton;
+        private System.Windows.Forms.Button openSessionButton;
+        private System.Windows.Forms.Button closeSessionButton;
+        private System.Windows.Forms.Button executeButton;
+        private CueTextBox loginTextBox;
+        private CueTextBox serverAdressTextBox;
+        private CueTextBox serverPortTextBox;
+        private System.Windows.Forms.Label connectionLabel;
+        private CueTextBox passwordTextBox;
     }
 }
 
--- Information_networks/Form1.cs
+++ Information_networks/Form1.cs
@@ -1,20 +1,75 @@
-﻿using System;
+﻿using Npgsql;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
-using System.Data;
 using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace Information_networks
 {
     public partial class Form1 : Form
     {
+        private static string host;
+        private static string user = "postgres";
+        private static string dbName = "InformationNetworks";
+        private static string port;
+        private static string password = "hugelong123";
+        private static string connString;
+        private static NpgsqlConnection conn;
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void initButtonClick(object sender, EventArgs e)
+        {
+            // Set visible TBoxes
+            loginTextBox.Visible = true;
+            passwordTextBox.Visible = true;
+            serverAdressTextBox.Visible = true;
+            serverPortTextBox.Visible = true;
+        }
+
+        private void openSessionButtonClick(object sender, EventArgs e)
+        {
+            // Get host and port from TBoxes
+            host = serverAdressTextBox.Text;
+            port = serverPortTextBox.Text;
+            connString = String.Format(
+                "Server={0}; Username={1}; Database={2}; Port={3}; Password={4}; SSLMode=Prefer",
+                host,
+                user,
+                dbName,
+                port,
+                password);
+            // Connect to DB
+            conn = new NpgsqlConnection(connString);
+            conn.Open();
+            connectionLabel.Text = "Подключено";
+            connectionLabel.ForeColor = Color.Green;
+            executeButton.Visible = true;
+            // Execute INSERT in PGSQL
+            using (var command = new NpgsqlCommand("INSERT INTO users(username, password) VALUES(@u, @p)", conn))
+            {
+                command.Parameters.AddWithValue("u", loginTextBox.Text);
+                command.Parameters.AddWithValue("p", passwordTextBox.Text);
+                command.ExecuteNonQuery();
+            }
+        }
+
+        private void closeSessionButtonClick(object sender, EventArgs e)
+        {
+            // Close connection
+            conn.Close();
+            executeButton.Visible = false;
+            connectionLabel.Text = "Не подключено";
+            connectionLabel.ForeColor = Color.Red;
+        }
+
+        private void executeButtonClick(object sender, EventArgs e)
+        {
+            // What is here?
         }
     }
 }
