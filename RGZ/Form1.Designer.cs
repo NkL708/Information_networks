2fc635cae877137ed0235f810b317ec814089a5a
NkL <nklnsk708@gmail.com>
2022-05-31 00:16:10 +0700

    kill me please

---
+++ RGZ/Form1.Designer.cs
@@ -0,0 +1,166 @@
+﻿namespace RGZ
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView = new System.Windows.Forms.DataGridView();
+            this.teachersButton = new System.Windows.Forms.Button();
+            this.disciplinesButton = new System.Windows.Forms.Button();
+            this.departmentDisciplinesButton = new System.Windows.Forms.Button();
+            this.teachersOfDisciplineButton = new System.Windows.Forms.Button();
+            this.teacherReferenceButton = new System.Windows.Forms.Button();
+            this.teacherIdtextBox = new System.Windows.Forms.TextBox();
+            this.disciplineIdtextBox = new System.Windows.Forms.TextBox();
+            this.updateTableButton = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView
+            // 
+            this.dataGridView.BackgroundColor = System.Drawing.Color.White;
+            this.dataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView.Location = new System.Drawing.Point(185, 21);
+            this.dataGridView.Name = "dataGridView";
+            this.dataGridView.Size = new System.Drawing.Size(603, 326);
+            this.dataGridView.TabIndex = 0;
+            // 
+            // teachersButton
+            // 
+            this.teachersButton.BackColor = System.Drawing.Color.Aqua;
+            this.teachersButton.Location = new System.Drawing.Point(18, 21);
+            this.teachersButton.Name = "teachersButton";
+            this.teachersButton.Size = new System.Drawing.Size(150, 23);
+            this.teachersButton.TabIndex = 1;
+            this.teachersButton.Text = "Преподаватели";
+            this.teachersButton.UseVisualStyleBackColor = false;
+            this.teachersButton.Click += new System.EventHandler(this.TeachersButtonClick);
+            // 
+            // disciplinesButton
+            // 
+            this.disciplinesButton.BackColor = System.Drawing.Color.Aqua;
+            this.disciplinesButton.Location = new System.Drawing.Point(18, 63);
+            this.disciplinesButton.Name = "disciplinesButton";
+            this.disciplinesButton.Size = new System.Drawing.Size(150, 23);
+            this.disciplinesButton.TabIndex = 2;
+            this.disciplinesButton.Text = "Дисциплины";
+            this.disciplinesButton.UseVisualStyleBackColor = false;
+            this.disciplinesButton.Click += new System.EventHandler(this.DisciplinesButtonClick);
+            // 
+            // departmentDisciplinesButton
+            // 
+            this.departmentDisciplinesButton.BackColor = System.Drawing.Color.Aqua;
+            this.departmentDisciplinesButton.Location = new System.Drawing.Point(18, 103);
+            this.departmentDisciplinesButton.Name = "departmentDisciplinesButton";
+            this.departmentDisciplinesButton.Size = new System.Drawing.Size(150, 23);
+            this.departmentDisciplinesButton.TabIndex = 3;
+            this.departmentDisciplinesButton.Text = "Дисциплины кафедры";
+            this.departmentDisciplinesButton.UseVisualStyleBackColor = false;
+            this.departmentDisciplinesButton.Click += new System.EventHandler(this.DepartmentDisciplinesButtonClick);
+            // 
+            // teachersOfDisciplineButton
+            // 
+            this.teachersOfDisciplineButton.BackColor = System.Drawing.Color.Aqua;
+            this.teachersOfDisciplineButton.Location = new System.Drawing.Point(12, 298);
+            this.teachersOfDisciplineButton.Name = "teachersOfDisciplineButton";
+            this.teachersOfDisciplineButton.Size = new System.Drawing.Size(156, 23);
+            this.teachersOfDisciplineButton.TabIndex = 4;
+            this.teachersOfDisciplineButton.Text = "Преподаватели дисциплины";
+            this.teachersOfDisciplineButton.UseVisualStyleBackColor = false;
+            // 
+            // teacherReferenceButton
+            // 
+            this.teacherReferenceButton.BackColor = System.Drawing.Color.Aqua;
+            this.teacherReferenceButton.Location = new System.Drawing.Point(12, 227);
+            this.teacherReferenceButton.Name = "teacherReferenceButton";
+            this.teacherReferenceButton.Size = new System.Drawing.Size(156, 23);
+            this.teacherReferenceButton.TabIndex = 5;
+            this.teacherReferenceButton.Text = "Справка по преподавателю";
+            this.teacherReferenceButton.UseVisualStyleBackColor = false;
+            // 
+            // teacherIdtextBox
+            // 
+            this.teacherIdtextBox.Location = new System.Drawing.Point(12, 256);
+            this.teacherIdtextBox.Name = "teacherIdtextBox";
+            this.teacherIdtextBox.Size = new System.Drawing.Size(156, 20);
+            this.teacherIdtextBox.TabIndex = 6;
+            // 
+            // disciplineIdtextBox
+            // 
+            this.disciplineIdtextBox.Location = new System.Drawing.Point(12, 327);
+            this.disciplineIdtextBox.Name = "disciplineIdtextBox";
+            this.disciplineIdtextBox.Size = new System.Drawing.Size(156, 20);
+            this.disciplineIdtextBox.TabIndex = 7;
+            // 
+            // updateTableButton
+            // 
+            this.updateTableButton.BackColor = System.Drawing.Color.Aqua;
+            this.updateTableButton.Location = new System.Drawing.Point(18, 141);
+            this.updateTableButton.Name = "updateTableButton";
+            this.updateTableButton.Size = new System.Drawing.Size(150, 23);
+            this.updateTableButton.TabIndex = 8;
+            this.updateTableButton.Text = "Обновить таблицу";
+            this.updateTableButton.UseVisualStyleBackColor = false;
+            this.updateTableButton.Click += new System.EventHandler(this.UpdateTableButtonClick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Lime;
+            this.ClientSize = new System.Drawing.Size(799, 362);
+            this.Controls.Add(this.updateTableButton);
+            this.Controls.Add(this.disciplineIdtextBox);
+            this.Controls.Add(this.teacherIdtextBox);
+            this.Controls.Add(this.teacherReferenceButton);
+            this.Controls.Add(this.teachersOfDisciplineButton);
+            this.Controls.Add(this.departmentDisciplinesButton);
+            this.Controls.Add(this.disciplinesButton);
+            this.Controls.Add(this.teachersButton);
+            this.Controls.Add(this.dataGridView);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView;
+        private System.Windows.Forms.Button teachersButton;
+        private System.Windows.Forms.Button disciplinesButton;
+        private System.Windows.Forms.Button departmentDisciplinesButton;
+        private System.Windows.Forms.Button teachersOfDisciplineButton;
+        private System.Windows.Forms.Button teacherReferenceButton;
+        private System.Windows.Forms.TextBox teacherIdtextBox;
+        private System.Windows.Forms.TextBox disciplineIdtextBox;
+        private System.Windows.Forms.Button updateTableButton;
+    }
+}
+
---
+++ RGZ/Form1.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using System.Windows.Forms;
+using Npgsql;
+
+namespace RGZ
+{
+    public partial class Form1 : Form
+    {
+        private static string host = "localhost";
+        private static readonly string user = "postgres";
+        private static readonly string dbName = "IN_RGZ";
+        private static readonly string port = "5432";
+        private static readonly string password = "hugelong123";
+        private static string conString;
+        private static NpgsqlConnection connection;
+        private static string currentTable;
+
+        public Form1()
+        {
+            InitializeComponent();
+            Connect();
+        }
+
+        private static void Connect()
+        {
+            conString = $"Server={host}; Username={user}; Database={dbName}; Port={port}; Password={password}; SSLMode=Prefer";
+            connection = new NpgsqlConnection(conString);
+            connection.Open();
+        }
+        private void TeachersButtonClick(object sender, EventArgs e)
+        {
+            using (NpgsqlDataAdapter adapter = new NpgsqlDataAdapter(
+                "SELECT id, last_name, first_name, patronymic,\n" +
+                "department_name, employment_date\n" +
+                "FROM teachers;", connection))
+            {
+                DataTable dataTable = new DataTable();
+                adapter.Fill(dataTable);
+                dataGridView.DataSource = dataTable;
+            }
+            currentTable = "teachers";
+        }
+
+        private void DisciplinesButtonClick(object sender, EventArgs e)
+        {
+            using (NpgsqlDataAdapter adapter = new NpgsqlDataAdapter(
+                "SELECT id, name, speciality, semester\n" +
+                "FROM disciplines", connection))
+            {
+                DataTable dataTable = new DataTable();
+                adapter.Fill(dataTable);
+                dataGridView.DataSource = dataTable;
+            }
+            currentTable = "disciplines";
+        }
+
+        private void DepartmentDisciplinesButtonClick(object sender, EventArgs e)
+        {
+            using (NpgsqlDataAdapter adapter = new NpgsqlDataAdapter(
+                "SELECT id, teacher_id, discipline_id, hours_count\n" +
+                "FROM department_disciplines", connection))
+            {
+                DataTable dataTable = new DataTable();
+                adapter.Fill(dataTable);
+                dataGridView.DataSource = dataTable;
+            }
+            currentTable = "department_disciplines";
+        }
+
+        private void UpdateTableButtonClick(object sender, EventArgs e)
+        {
+            DataTable table = (DataTable)dataGridView.DataSource;
+            if (table == null)
+                return;
+            DataTable newTable = new DataTable();
+            string colsStr = "";
+            foreach (DataColumn column in table.Columns)
+            {
+                newTable.Columns.Add(column.ColumnName);
+                colsStr += $"@{column}, ";
+            }
+            colsStr = colsStr.Remove(colsStr.Length - 2);
+            foreach (DataRow row in table.Rows)
+            {
+                string valuesStr = "";
+                foreach (object cell in row.ItemArray)
+                {
+                    bool isDate = DateTime.TryParse(cell.ToString(), out DateTime date);
+                    if (isDate)
+                        valuesStr += $"{date:yyyy-MM-dd}, ";
+                    else
+                        valuesStr += $"{cell}, ";
+                }
+                valuesStr = valuesStr.Remove(valuesStr.Length - 2);
+                using (NpgsqlCommand command = new NpgsqlCommand(
+                    $"INSERT INTO {currentTable}({colsStr})\n" +
+                    $"VALUES({valuesStr});", connection))
+                {
+                    string[] cols = colsStr.Split();
+                    string[] values = valuesStr.Split();
+                    var colsAndValues = cols.Zip(values, (c, v) => new { Col = c, Value = v });
+                    foreach (var cv in colsAndValues)
+                    {
+                        if (cv.Col.Contains(",") || cv.Value.Contains(","))
+                            command.Parameters.AddWithValue(cv.Col.Trim(','), cv.Value.Trim(','));
+                        else
+                            command.Parameters.AddWithValue(cv.Col, cv.Value);
+                    }
+                    try
+                    {
+                        Debug.WriteLine($"{command.CommandText}\n");
+                        command.ExecuteNonQuery();
+                        newTable.Rows.Add(row.ItemArray);
+                    }
+                    // If already in DB
+                    catch (NpgsqlException) { }
+                }
+            }
+            dataGridView.DataSource = newTable;
+        }
+    }
+}
